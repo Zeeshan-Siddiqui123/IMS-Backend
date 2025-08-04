dd95713711780aef90637bf37c205937ee83ca4d
Zeeshan Siddiqui <zeeshansd767@gmail.com>
2025-08-04 17:49:56 +0500

    pm model pm controller

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,10 +2,18 @@ const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postcontroller");
 const upload = require("../config/multerconfig");
+const PMcontroller = require("../controllers/PMcontroller");
 
+//admin access posts
 router.post("/post", upload.single('image'), postController.createPost)
 router.get("/post", postController.getPosts)
 router.put("/post/:id", upload.single('image'), postController.updatePost)
 router.delete("/post/:id", postController.deletePost)
+
+//admin access PMs
+router.post("/pm", PMcontroller.createPM)
+router.get("/pm", PMcontroller.getPMs)
+router.put("/pm/:id", PMcontroller.updatePM)
+router.delete("/pm/:id", PMcontroller.deletePM)
 
 module.exports = router;
